8f8f36e79d1914f6759973ddf0eb18bcf020de22
jrpcoder <josepereira92@gmail.com>
2017-06-23 22:26:23 +0100

    create submit and api call action creators

---
+++ weather-app/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY="5a39b0aec734f7b26922b99ef6b70726";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	let url = `${ROOT_URL}&q=${city}`;
+	let request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
