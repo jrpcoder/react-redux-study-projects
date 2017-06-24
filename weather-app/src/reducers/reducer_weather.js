17b6aae92b636e71911ea6f1af6a793e026d2545
jrpcoder <josepereira92@gmail.com>
2017-06-24 10:17:34 +0100

    add conditional to update state only when payload is valid

--- weather-app/src/reducers/reducer_weather.js
+++ weather-app/src/reducers/reducer_weather.js
@@ -1,9 +1,12 @@
 import {FETCH_WEATHER} from '../actions/index';
 
 export default function weatherReducer(state=[], action){
-	switch (action.type) {
-	case FETCH_WEATHER:
-		return [action.payload.data, ...state];
+	if(action.payload) {
+		switch (action.type) {
+		case FETCH_WEATHER:
+			console.log('result3:', action.payload);
+			return [action.payload.data, ...state];
+		}
 	}
 	return state;
 }
