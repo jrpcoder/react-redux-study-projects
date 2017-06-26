bc6c5af2cc8d2eef16db115975d1af4cec0f29df
jrpcoder <josepereira92@gmail.com>
2017-06-26 23:01:48 +0100

    Update

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Component1 from './components/Component1.js';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
 
-ReactDOM.render(<Component1 />, document.getElementById('root'));
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)} >
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route path="/posts/new" component={PostsNew} />
+					<Route path="/" component={PostsIndex} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>
+	, document.getElementById('root'));
