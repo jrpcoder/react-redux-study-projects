4c41f736966e36a82cc18a7186c05031ad9d4183
jrpcoder <josepereira92@gmail.com>
2017-06-29 23:25:30 +0100

    added validation step

--- blog-app/src/components/posts_new.js
+++ blog-app/src/components/posts_new.js
@@ -13,6 +13,7 @@ class PostsNew extends Component {
 					{...field.input}
 				/>
 			</div>
+			{field.meta.error}
 		);
 	}
 
@@ -25,8 +26,8 @@ class PostsNew extends Component {
 					component={this.renderField}
 				/>
 				<Field
-					label="Tags"
-					name="tags"
+					label="Categories"
+					name="categories"
 					component={this.renderField}
 				/>
 				<Field
@@ -39,6 +40,25 @@ class PostsNew extends Component {
 	}
 }
 
+function validate(values){
+	const errors = {};
+
+	// validate each property of values
+	if (!values.title) {
+		errors.title = 'Enter a title';
+	}
+
+	if (!values.categories) {
+		errors.categories = 'Enter some categories';
+	}
+
+	if (!values.content) {
+		errors.content = 'Include some content';
+	}
+	return errors;
+}
+
 export default reduxForm({
+	validate,
 	form: 'PostsNewForm'
 })(PostsNew);
