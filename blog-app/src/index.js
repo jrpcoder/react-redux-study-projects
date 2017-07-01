e1c41ce724d9e9a0c0b5d540ccec40f2efb296e0
jrpcoder <josepereira92@gmail.com>
2017-07-01 19:22:15 +0100

    add new route to post show

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -8,15 +8,17 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostShow from './components/post_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)} >
 		<BrowserRouter>
-			<div>
+			<div className="position1">
 				<Switch>
 					<Route path="/posts/new" component={PostsNew} />
+					<Route path="/posts/:id" component={PostShow} />
 					<Route path="/" component={PostsIndex} />
 				</Switch>
 			</div>
