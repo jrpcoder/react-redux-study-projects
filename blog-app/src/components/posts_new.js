61bee5a934830ddb0ef6401bb380fac9779e2f9c
jrpcoder <josepereira92@gmail.com>
2017-07-01 19:29:29 +0100

    add onSubmit feature and error/style logic to input fields

--- blog-app/src/components/posts_new.js
+++ blog-app/src/components/posts_new.js
@@ -1,25 +1,40 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
 	
 	renderField(field) {
+		const {meta: {touched, error}} = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input
 					className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>
-			{field.meta.error}
 		);
 	}
 
+	onSubmit(values) {
+		this.props.createPost(values, () => {
+			this.props.history.push("/");
+		});
+	}
+
 	render() {
+		const {handleSubmit} = this.props;
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
 					label="Title"
 					name="title"
@@ -35,6 +50,8 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
@@ -61,4 +78,6 @@ function validate(values){
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null, {createPost})(PostsNew)
+);
